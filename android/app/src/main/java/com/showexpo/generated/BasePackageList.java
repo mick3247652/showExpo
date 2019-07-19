afe87daa8cf0d97eb5a98a7fa2a7efe2ebaf29ce
mick3247652 <mick.dunaev@yandex.ru>
2019-07-19 13:11:47 +0300

    webview

--- android/app/src/main/java/com/showexpo/generated/BasePackageList.java
+++ android/app/src/main/java/com/showexpo/generated/BasePackageList.java
@@ -8,6 +8,7 @@ public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
         new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.contacts.ContactsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
